365c3f9db58c788a77bece522a7e739d0e1e8643
David <apolo4748@gmail.com>
2024-04-16 21:11:43 -0500

    falta while

--- assets/app.js
+++ assets/app.js
@@ -14,10 +14,25 @@ if (gastoTotal>presupuesto){
     let diasViaje = Number(prompt("su tiempo de estadia supera su presupesto, porfavor recorte sus dias"))
 
 }
+let seguir = confirm("quieres seguir?")
+while (seguir){
 if (gastoTotal<presupuesto){
     let restoPresupuesto = presupuesto - gastoTotal;
     let confirmacion = confirm("tienes " + restoPresupuesto + " ¿quieres aumentar tu tiempo de estadia?")
-        if (confirmacion === true){
+        if (confirmacion){
             let diasExtra = Number(prompt("ingrese la nueva cantidad de dias que se quiere hospedar"))
+            let gastoExtra = diasExtra * gastoDiario;
+            if(gastoExtra > restoPresupuesto){
+                let diasExtra = Number(prompt("la nueva cantidad de dias supera al presupuesto, porfavor elije menos dias"))
+                alert("nueva cantidad de dias actializado, disfrute sus vacaciones")
+            }
+            if(gastoExtra < restoPresupuesto){
+                alert("la nueva cantidad de dias esta dentro del presupuesto, disfrute sus vacaciones")
+            }
         }
+}
+let seguir = confirm("quieres seguir?")
+
+
+break;
 }
